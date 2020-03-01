28c90c47488a906c40fef61f46fcca4087e9fb4e
ozubergz <jmin499@gmail.com>
2020-03-01 16:58:10 -0500

    change background

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,18 @@ const Home = () => {
         <div>
             <NavBar />
             <div className="home-bg-1">
-                <div className="bg-1-parallax"></div>
+                <div className="bg-1-parallax">
+
+                    <div className="title-wrapper">
+                        <div className="page-title-wrapper">
+                            <h1 className="page-title">Fly Credit</h1>
+                        </div>
+                        <div className="page-desc-wrapper">
+                            <h4 className="page-desc">Build Your Business</h4>
+                        </div>
+                    </div>
+
+                </div>
             </div>
             <div style={{height: '1000px'}}></div>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import { Menu } from 'semantic-ui-react';
 
-class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg fixed-top navbar-dark">
-                    <a className="navbar-brand" href="#">LOGO</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Blogs</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg fixed-top navbar-dark">
+                <a className="navbar-brand" href="#">LOGO</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item mr-4">
+                            <a className="nav-link" href="#">HOME</a>
+                        </li>
+                        <li className="nav-item mr-4">
+                            <a className="nav-link" href="#">BLOGS</a>
+                        </li>
+                        <li className="nav-item mr-4">
+                            <a className="nav-link" href="#">ABOUT</a>
+                        </li>
+                        <li className="nav-item mr-4">
+                            <a className="nav-link" href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default NavBar;
